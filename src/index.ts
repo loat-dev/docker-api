7f12c73e2594f3b1c2bbd6bd8a41ba35ba5eb025
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-11-10 13:26:19 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { Socket } from './socket/socket.ts';
 
+const socket = new Socket('/var/run/docker.sock');
+
+
+socket.request("/containers/json")
+  .then(res => res.json())
+  .then(json => console.log(json));
